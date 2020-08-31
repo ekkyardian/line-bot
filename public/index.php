edd85c8cebbb790f352c31b2715eb52574dcda15
ekkyardian <ekkyardian@outlook.com>
2020-08-31 14:17:38 +0700

    Add Feature: Reply Message

--- public/index.php
+++ public/index.php
@@ -15,8 +15,8 @@ use \LINE\LINEBot\SignatureValidator as SignatureValidator;
 $pass_signature = true;
 
 // set LINE channel_access_token and channel_secret
-$channel_access_token = "";
-$channel_secret = "";
+$channel_access_token = "Dc8QoO/qrRi1saJoARND7/Bs7YYfRit/1FqRhLSdjPdpAOfigXsI6KDm0fBu7aW7HAhMYGKSVllZUkbsamNMrcTGv1snR2arkfr0rOtSh4erFUECFlYxAgPT9H3RRfW48aFyy13bG1f0t3pBY2mRsQdB04t89/1O/w1cDnyilFU=";
+$channel_secret = "33d217f440355d89070172e98e19b581";
  
 // inisiasi objek bot
 $httpClient = new CurlHTTPClient($channel_access_token);
@@ -31,7 +31,7 @@ $app->get('/', function (Request $request, Response $response, $args) {
 });
  
 // buat route untuk webhook
-$app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
+$app->post('https://line-bot-ekky.herokuapp.com/', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
     // get request body and line signature header
     $body = $request->getBody();
     $signature = $request->getHeaderLine('HTTP_X_LINE_SIGNATURE');
@@ -51,7 +51,27 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
         }
     }
 
-// kode aplikasi nanti disini
+    // kode aplikasi nanti disini
+    $data = json_decode($body, true);
+    if(is_array($data['events'])){
+        foreach ($data['events'] as $event) {
+            if ($event['type'] == 'message') {
+                if($event['message']['type'] == 'text') {
+                    // send same message as reply to user
+                    $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+
+                    // or we can use replyMessage() instead to send reply message
+                    // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                    // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+
+                    $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus($result->getHTTPStatus());
+                }
+            }
+        }
+    }
  
 });
 $app->run();
