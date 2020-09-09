405d74bb575c998916c7a8044d78e9f0e5cd5942
ekkyardian <ekkyardian@outlook.com>
2020-09-10 02:33:51 +0700

    :hammer: Still trying! I'm so done with this..[2]

--- public/index.php
+++ public/index.php
@@ -52,26 +52,26 @@ $app->post('https://line-bot-ekky.herokuapp.com/public/webhook', function (Reque
     }
 
     // kode aplikasi nanti disini
-    // $data = json_decode($body, true);
-    // if(is_array($data['events'])){
-    //     foreach ($data['events'] as $event) {
-    //         if ($event['type'] == 'message') {
-    //             if($event['message']['type'] == 'text') {
-    //                 // send same message as reply to user
-    //                 $result = $bot->replyText($event['replyToken'], 'Congratulation!');
+    $data = json_decode($body, true);
+    if(is_array($data['events'])){
+        foreach ($data['events'] as $event) {
+            if ($event['type'] == 'message') {
+                if($event['message']['type'] == 'text') {
+                    // send same message as reply to user
+                    $result = $bot->replyText($event['replyToken'], 'Congratulation!');
 
-    //                 // or we can use replyMessage() instead to send reply message
-    //                 // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
-    //                 // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                    // or we can use replyMessage() instead to send reply message
+                    // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                    // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 
-    //                 $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
-    //                 return $response
-    //                     ->withHeader('Content-Type', 'application/json')
-    //                     ->withStatus($result->getHTTPStatus());
-    //             }
-    //         }
-    //     }
-    // }
+                    $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus($result->getHTTPStatus());
+                }
+            }
+        }
+    }
  
 });
 $app->run();
